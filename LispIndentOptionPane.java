2be04e69fad2b3d31bd03a7c4b85e90f08ecea5c
Jonathan Fischer Friberg <odyssomay@gmail.com>
2012-10-14 00:19:27 +0200

    file endings option

--- LispIndentOptionPane.java
+++ LispIndentOptionPane.java
@@ -2,11 +2,13 @@
 import org.gjt.sp.jedit.AbstractOptionPane;
 import org.gjt.sp.jedit.jEdit;
 
-
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 import javax.swing.JCheckBox;
 
 public class LispIndentOptionPane extends AbstractOptionPane {
 	JCheckBox check_ending;
+	JTextField file_endings;
 	
 	public LispIndentOptionPane() { super("lispindent"); }
 	
@@ -14,11 +16,17 @@ public class LispIndentOptionPane extends AbstractOptionPane {
 		//org.gjt.sp.util.Log.log(org.gjt.sp.util.Log.DEBUG, null, "starting option pane");
 		check_ending = new JCheckBox("Check file ending before indenting",
 			jEdit.getBooleanProperty(LispIndentPlugin.OPTIONS_PREFIX + "check_ending"));
+		file_endings = new JTextField(jEdit.getProperty(
+			LispIndentPlugin.OPTIONS_PREFIX + "file_endings"), 25);
 		addComponent(check_ending);
+		addComponent(new JLabel("Lisp file endings (delimited by |)"));
+		addComponent(file_endings);
 	}
 	
 	public void _save() {
 		jEdit.setBooleanProperty(LispIndentPlugin.OPTIONS_PREFIX + "check_ending",
 			check_ending.isSelected());
+		jEdit.setProperty(LispIndentPlugin.OPTIONS_PREFIX + "file_endings",
+			file_endings.getText());
 	}
 }
