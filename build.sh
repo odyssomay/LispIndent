4d55adb1181d20d5cb7de5743a8aa9f87e147fd5
Jonathan Fischer Friberg <odyssomay@gmail.com>
2012-10-17 13:36:25 +0200

    include all class files in jar

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 javac -cp /usr/share/jedit/jedit.jar LispIndentPlugin.java LispIndentOptionPane.java
-jar cf LispIndent.jar LispIndentPlugin.class LispIndentOptionPane.class LispIndent.props actions.xml
+#jar cf LispIndent.jar LispIndentPlugin.class LispIndentOptionPane.class LispIndentOptionPane\$CheckEndingListener.class LispIndent.props actions.xml
+jar cf LispIndent.jar *.class LispIndent.props actions.xml
 cp LispIndent.jar ~/.jedit/jars/
