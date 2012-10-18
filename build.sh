4e5210a79e77d6233cecdd23e05a2ede0282dbeb
Jonathan Fischer Friberg <odyssomay@gmail.com>
2012-10-18 20:53:59 +0200

    cleanup before building

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm *.class *.jar
 javac -cp /usr/share/jedit/jedit.jar LispIndentPlugin.java LispIndentOptionPane.java LispIndentPreset.java LispIndentPresets.java
 jar cf LispIndent.jar *.class LispIndent.props actions.xml
 cp LispIndent.jar ~/.jedit/jars/
